2098e9219d72ef78522a208386e33fdb5b8b9a67
Emman77240 <asiegbuemmanuelekee@gmail.com>
2022-02-27 15:51:12 +0000

    Refactored almostIncreasingSequence function

--- almostIncreasingSequence/almostIncreasingSequence.js
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -1,14 +1,22 @@
 function almostIncreasingSequence(inputArr) {
-    let count = 0
+    let count = 0;
+    let arrLen = inputArr.length;
 
-    for(let i = 0; i < inputArr.length - 1; i++) {
-        if(inputArr[i] > inputArr[i + 1]) count++;
-    
+    for(let i = 0; i < arrLen - 1; i++) {
+        if(inputArr[i] > inputArr[i + 1]) {
+            count++;
+            let num = 0;
+            for(let j = 0; j < arrLen; j++) {
+                if(inputArr[i] === inputArr[j]) num++;
+            }
+
+            if(num > 2) return false;
+        }
     }
-    return count + 1 === inputArr.length? true : false;
+    return count <= 1;
 
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1]))
+console.log(almostIncreasingSequence([1, 3, 2, 1]))
 
 module.exports = almostIncreasingSequence;
--- almostIncreasingSequence/almostIncreasingSequence.test.js
+++ almostIncreasingSequence/almostIncreasingSequence.test.js
@@ -2,14 +2,14 @@ const almostIncreasingSequence = require("./almostIncreasingSequence");
 
 describe("Almost Increasing Sequence", () => {
     test("First test for almost increasing sequence in array", () => {
-        const data = [3, 2, 1];
-
-        expect(almostIncreasingSequence(data)).toBe(true);
-    });
-
-    test("Second test for almost increasing sequence in array", () => {
         const data = [1, 3, 2, 1];
 
         expect(almostIncreasingSequence(data)).toBe(false);
+    });
+
+    test("Second test for almost increasing sequence in array", () => {
+        const data = [1, 3, 2];
+
+        expect(almostIncreasingSequence(data)).toBe(true);
     });
 })
